ba9c0b30ee61564439838c98fdcee0a981d29a0d
zslavanya <lavanya.k@zelarsoft.com>
2021-04-25 23:52:44 +0530

    addind sample.txt file

--- 06-function.sh
+++ 06-function.sh
@@ -10,12 +10,14 @@ sample2() {
   echo Hello from Function2
 
   echo First Argument in Function = $1
+  return 100
 }
 ## Access the function
 
 a=10
 sample1
 sample2 200
+echo exit status = $?
 echo b - $b
 
 echo First Argument = $1
