fe8518aa0d2afcabad762577a64f64d80eb0412e
zslavanya <lavanya.k@zelarsoft.com>
2021-04-25 23:40:58 +0530

    addind sample.txt file

--- 06-function.sh
+++ 06-function.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 ##Declar the function
 function sample1(){
+  echo a - $a
   echo Hello from Function1
 }
 
@@ -8,5 +9,7 @@ sample2() {
   echo Hello from Function2
 }
 ## Access the function
+
+a=10
 sample1
 sample2
