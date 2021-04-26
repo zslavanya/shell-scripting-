4be5bac8cea16157f46dd315658ae48daf12ca1f
zslavanya <lavanya.k@zelarsoft.com>
2021-04-26 22:23:02 +0530

    addind sample.txt file

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -3,9 +3,17 @@
 COMPONENT=$1
 
 if [ ! -f components/${COMPONENT}.sh ]; then
-  echo "Invalid component name is provided"
+  echo -e "\e[31mInvalid component name is provided\e[0m"
   exit 1
 fi
+
+USER_NAME=$(whoami)
+
+if [ "${USER_NAME}" != "root"]; then
+  echo "You should be a root user to perform this script, Try with sudo "
+  exit 1
+fi
+
 export COMPONENT
 bash components/${COMPONENT}.sh
 
