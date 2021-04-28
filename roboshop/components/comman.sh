22757d1e0384adf5e9c8c226950b441664407fd6
zslavanya <lavanya.k@zelarsoft.com>
2021-04-28 11:17:46 +0530

    addind sample.txt file

--- roboshop/components/comman.sh
+++ roboshop/components/comman.sh
@@ -1,12 +1,15 @@
 #!bin/bash
 
 OS_PREREQ() {
+  LOG=/tmp/${COMPONENT}.log
+  rm -f $LOG
   set-hostname ${component}
-  apt update
+  apt update &>$LOG
 }
 
 ERROR(){
   echo -e "\e[1;31m$1\e[0m"
 }
+
 
 
